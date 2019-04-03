cb7886f1c30087fd02a1860e4d41ce1d332f0770
Rishi0721 <rishmikalavandi@gmail.com>
2019-04-03 08:44:14 +0530

    error solve

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,15 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FoodMenuModule } from './food-menu/food-menu.module';
 import { FoodmenuModule } from './foodmenu/foodmenu.module';
+
 
 @NgModule({
   declarations: [AdminComponent, DashboardComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    FoodMenuModule,
     FoodmenuModule
   ]
 })
