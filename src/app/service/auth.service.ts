7e6e08e20c0167f86828ebb1958bed151086c825
Rishi0721 <rishmikalavandi@gmail.com>
2019-04-03 14:26:43 +0530

    signin page

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -8,37 +8,38 @@ providedIn: 'root'
 })
 export class AuthService {
 userData: any; // Save logged in user data
-constructor(
-public afs: AngularFirestore,   // Inject Firestore service
-public afAuth: AngularFireAuth, // Inject Firebase auth service
-public router: Router,  
-public ngZone: NgZone // NgZone service to remove outside scope warning
-) {    
+    constructor(
+        public afs: AngularFirestore,   // Inject Firestore service
+        public afAuth: AngularFireAuth, // Inject Firebase auth service
+        public router: Router,  
+        public ngZone: NgZone // NgZone service to remove outside scope warning
+    )
+    {    
 /* Saving user data in localstorage when 
 logged in and setting up null when logged out */
-this.afAuth.authState.subscribe(user => {
-if (user) {
-this.userData = user;
-localStorage.setItem('user', JSON.stringify(this.userData));
-JSON.parse(localStorage.getItem('user'));
-} else {
-localStorage.setItem('user', null);
-JSON.parse(localStorage.getItem('user'));
-}
-})
-}
+    this.afAuth.authState.subscribe(user => {
+        if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user'));
+        } else {
+        localStorage.setItem('user', null);
+        JSON.parse(localStorage.getItem('user'));
+        }
+    })
+    }
 // Sign in with email/password
-SignIn(email, password) {
-return this.afAuth.auth.signInWithEmailAndPassword(email, password)
-.then((result) => {
-this.ngZone.run(() => {
-this.router.navigate(['']);
-});
-this.SetUserData(result.user);
-}).catch((error) => {
-window.alert(error.message)
-})
-}
+    SignIn(email, password) {
+        return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+        .then((result) => {
+        this.ngZone.run(() => {
+        this.router.navigate(['']);
+        });
+        this.SetUserData(result.user);
+        }).catch((error) => {
+        window.alert(error.message)
+        })
+    }
 // Sign up with email/password
 SignUp(email, password) {
 return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
@@ -73,6 +74,7 @@ const user = JSON.parse(localStorage.getItem('user'));
 return (user !== null && user.emailVerified !== false) ? true : false;
 }
 
+
 // Auth logic to run auth providers
 AuthLogin(provider) {
 return this.afAuth.auth.signInWithPopup(provider)
@@ -95,6 +97,7 @@ uid: user.uid,
 name :user.name,
 email: user.email,
 password : user.password,
+retypePassword : user.retypePassword,
 emailVerified: user.emailVerified
 }
 return userRef.set(userData, {
--- src/app/service/user.ts
+++ src/app/service/user.ts
@@ -3,5 +3,6 @@ export interface User {
     name : string;
     email : string;
     password :string;
+    retypePassword : string;
     emailVerified : boolean;
 }
